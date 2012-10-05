31d64f898b44205de4fbe7c4fd3d879ca828857e
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-05 12:26:00 +0000

    v1.0.1: better error messages
    git-svn-id: https://plugins.svn.wordpress.org/recent-facebook-posts/tags/1.0.1@608470 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/class-rfb-widget.php
@@ -0,0 +1,122 @@
+<?php
+
+class RFB_Widget extends WP_Widget {
+
+	public function __construct() {
+		parent::__construct(
+	 		'rfb_widget', // Base ID
+			'Recent Facebook Posts', // Name
+			array( 'description' => 'List your # most recent Facebook posts with this widget.' )
+		);
+	}
+
+ 	public function form( $instance ) {
+
+ 		$title = (isset($instance['title'])) ? $instance['title'] : 'Recent Facebook posts';
+ 		$number_of_posts = (isset($instance['number_of_posts'])) ? $instance['number_of_posts'] : 5;
+ 		$excerpt_length = (isset($instance['excerpt_length'])) ? $instance['excerpt_length'] : 140;
+ 		$show_like_count = (isset($instance['show_like_count']) && $instance['show_like_count']);
+ 		$show_comment_count = (isset($instance['show_comment_count']) && $instance['show_comment_count']);
+
+
+ 		global $RFB;
+ 		$rfb_options = $RFB->get_options();
+
+ 		if(empty($rfb_options['app_id'])) { ?>
+ 		<p style="color:red;">You'll need to <a href="<?php echo get_admin_url(null, 'options-general.php?page=rfb-settings'); ?>">configure Recent Facebook Posts</a> in order for it to work.</p>
+ 		<?php } ?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'number_of_posts' ); ?>"><?php _e( 'Number of posts:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'number_of_posts' ); ?>" name="<?php echo $this->get_field_name( 'number_of_posts' ); ?>" type="text" value="<?php echo esc_attr( $number_of_posts ); ?>" />
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'excerpt_length' ); ?>"><?php _e( 'Excerpt length:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'excerpt_length' ); ?>" name="<?php echo $this->get_field_name( 'excerpt_length' ); ?>" type="text" value="<?php echo esc_attr( $excerpt_length ); ?>" />
+		</p>
+
+		<p>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'show_like_count' ); ?>" name="<?php echo $this->get_field_name( 'show_like_count' ); ?>" value="1" <?php if($show_like_count) { ?>checked="1"<?php } ?> />
+			<label for="<?php echo $this->get_field_id( 'show_like_count' ); ?>"><?php _e( 'Show Like count?' ); ?></label> 
+		</p>
+
+		<p>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'show_comment_count' ); ?>" name="<?php echo $this->get_field_name( 'show_comment_count' ); ?>" value="1" <?php if($show_comment_count) { ?>checked="1"<?php } ?> />
+			<label for="<?php echo $this->get_field_id( 'show_comment_count' ); ?>"><?php _e( 'Show Comment count?' ); ?></label> 
+		</p>
+		<?php 
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['number_of_posts'] = (int) strip_tags( $new_instance['number_of_posts'] );
+		$instance['excerpt_length'] = (int) strip_tags($new_instance['excerpt_length']);
+		$instance['show_like_count'] = isset($new_instance['show_like_count']);
+		$instance['show_comment_count'] = isset($new_instance['show_comment_count']);
+		return $instance;
+	}
+
+	public function widget( $args, $instance ) {
+		global $RFB;
+
+		$posts = $RFB->get_posts();
+		$posts = array_slice($posts, 0, $instance['number_of_posts']);
+
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		echo $before_widget;
+		if ( ! empty( $title ) )
+			echo $before_title . $title . $after_title; ?>
+			<ul class="rfb_posts">
+			<?php foreach($posts as $post) { ?>
+				<li><?php echo nl2br(make_clickable(substr($post['content'], 0, $instance['excerpt_length']))); if(strlen($post['content']) > $instance['excerpt_length']) echo '..'; ?> 
+					<a target="_blank" class="fb_link" href="<?php echo $post['link']; ?>" rel="nofollow">
+						<?php if($instance['show_like_count'] || $instance['show_comment_count']) { ?><span class="like_count_and_comment_count"><?php } ?>
+						<?php if($instance['show_like_count']) { ?><span class="like_count"><?php echo $post['like_count']; ?> <span>likes</span></span> <?php } ?>
+						<?php if($instance['show_comment_count']) { ?><span class="comment_count"><?php echo $post['comment_count']; ?> <span>comments</span></span> <?php } ?>
+						<?php if($instance['show_like_count'] || $instance['show_comment_count']) { ?></span><?php } ?>
+						<span class="timestamp" title="<?php echo date('l, F j, Y', $post['timestamp']) . ' at ' . date('G:i', $post['timestamp']); ?>" ><?php if($instance['show_like_count'] || $instance['show_comment_count']) { ?> · <?php } ?><span><?php echo $this->time_ago($post['timestamp']); ?></span></span>
+					</a>
+				</li>
+			<?php } 
+
+			if(empty($posts)) { ?>
+				<li>No recent Facebook status updates to show.<?php if(current_user_can('manage_options')) { ?> Did you <a href="<?php echo get_admin_url(null,'options-general.php?page=rfb-settings'); ?>">configure the plugin</a> properly? (only admins see this sentence)<?php } ?></li>
+
+			<?php } ?>
+			</ul>
+			<?php 
+
+		echo $after_widget;
+	}
+
+	private function time_ago($timestamp) {
+		$diff = time() - (int) $timestamp;
+
+	    if ($diff == 0) 
+	         return 'just now';
+
+	    $intervals = array
+	    (
+	        1                   => array('year',    31556926),
+	        $diff < 31556926    => array('month',   2628000),
+	        $diff < 2629744     => array('week',    604800),
+	        $diff < 604800      => array('day',     86400),
+	        $diff < 86400       => array('hour',    3600),
+	        $diff < 3600        => array('minute',  60),
+	        $diff < 60          => array('second',  1)
+	    );
+
+	    $value = floor($diff/$intervals[1][1]);
+	    return $value.' '.$intervals[1][0].($value > 1 ? 's' : '').' ago';
+		    
+	}
+
+}
