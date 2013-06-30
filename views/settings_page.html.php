ff3a89ff84056c97916e10da38696612f337d7e4
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-30 15:53:22 +0000

    removed debugging statement on settings page
    git-svn-id: https://plugins.svn.wordpress.org/recent-facebook-posts/trunk@734328 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/settings_page.html.php
+++ views/settings_page.html.php
@@ -108,14 +108,18 @@
 		
 		<div id="rfb-donatebox" class="box">
 			<h3>Donate $10, $20 or $50</h3>
-			<p>I spent countless hours developing this plugin for <b>FREE</b>. If you like it, consider donating a token of your appreciation.</p>
+			<p>I spent countless hours developing this plugin for <b>FREE</b>. If you use it and like it, consider buying me a beer as a small token of your appreciation.</p>
 					
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-			<input type="hidden" name="cmd" value="_s-xclick">
-			<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHVwYJKoZIhvcNAQcEoIIHSDCCB0QCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCT0Lls0OHcadQPQYia2dXZvq5rcZoIJYFsQ+hi7hkeIfew8hVWTmXNm0Ozm4+QsmPge4dQB0kxne5sPYthMNi+Z2H7JhxYSusg2zE8EmZ5emuKuJJUXpOvy6isBrDI/bO5jLiaWfRY6m7MptgNFTmAk5aLJ38pndiC4d2HI7DgmDELMAkGBSsOAwIaBQAwgdQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIcG/KgGJG4+CAgbAZAk1u0H3M6NrQ2UlHx8riGOe7ude8UWx1uTc5Lz+xhxuFyrQTrqJxFeAiwE/3x255YytoqcpjqIk8DGeyG7pRCB9umy19b0msl6f+9jVucP964tYbQ5+yIyMNyG6qio31tTLHIQJlZr3Z/bMxcQVF0NGNdjLhz+tyzBKfEO6dw9zp+LrGYEeD2WtqjBOESvd4qxIG0sSbAeHWL+Kvv1LlrmfHcP54iNs0gX0A7vgnA6CCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTEyMDkyNjE0NTA1NlowIwYJKoZIhvcNAQkEMRYEFNb+ez+3/yYAFKTTJDenMAGeJ9wFMA0GCSqGSIb3DQEBAQUABIGACgD62tdwUiZEHvxXah0PD5Uhm7bczijYxM30zo2Yuidfsdx9au55zSS+Pps6gg0tfT513yekvaR2LKJv1fnOUZPAfe15/kOhD5HS8Xj+rtGW9ZZmVIFSEWMJSeU/22s3gNy8t0bUFjyFvYGkubhhskQ2KtEaZ9ixgW1VmvuORBY=-----END PKCS7-----
-			">
-			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-			<img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1">
+			<form class="donate" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+				<input type="hidden" name="cmd" value="_donations">
+				<input type="hidden" name="business" value="AP87UHXWPNBBU">
+				<input type="hidden" name="lc" value="US">
+				<input type="hidden" name="item_name" value="Danny van Kooten">
+				<input type="hidden" name="item_number" value="Recent Facebook Posts">
+				<input type="hidden" name="currency_code" value="USD">
+				<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHosted">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1">
 			</form>
 
 			<p>Or you can: </p>
