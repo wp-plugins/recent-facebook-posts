731b52a82e39fed2e3adac97f71a86119ef8cb3e
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-10 06:59:15 +0000

    v1.2.2
    git-svn-id: https://plugins.svn.wordpress.org/recent-facebook-posts/tags/1.2.2@738582 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/class-rfb.php
@@ -0,0 +1,329 @@
+<?php
+
+class RFB {
+
+	private static $instance;
+	private static $fb_instance;
+	private $defaults = array(
+		'app_id' => '',
+		'app_secret' => '',
+		'fb_id' => 'DannyvanKootenCOM',
+		'cache_time' => 1800,
+		'load_css' => 0,
+		'link_text' => 'Find us on Facebook',
+		'link_new_window' => 0,
+		'img_size' => 'thumbnail',
+		'img_width' => 100,
+		'img_height' => 100
+	);
+	private $options;
+
+	public static function get_instance() {
+		if(!self::$instance) self::$instance = new RFB();
+		return self::$instance;
+	}
+
+	public function __construct() {
+
+		add_action('wp_login', array($this, 'renew_access_token'));
+		add_action('init', array($this, 'on_init'));
+
+		add_shortcode('recent-facebook-posts', array($this, 'shortcode_output'));
+
+		// only on frontend
+		if(!is_admin()) {
+			$opts = $this->get_options();
+			if($opts['load_css']) {
+				add_action( 'wp_enqueue_scripts', array(&$this, 'load_css'));
+			}
+		}
+	}
+
+	public function on_init() {
+		if(!session_id() && !headers_sent()) {
+			session_start();
+		}
+	}
+
+	public function load_css() {
+		wp_register_style('rfb_css', plugins_url('recent-facebook-posts/css/rfb.css') );
+        wp_enqueue_style('rfb_css' );
+	}
+
+	public function get_options() {
+		if(!$this->options) {
+			$this->options = array_merge($this->defaults, (array) get_option('rfb_settings'));
+		}
+
+		return $this->options;
+	}
+
+	public function renew_access_token($redirect_uri = null) {
+
+		// only renew when the visitor is an administrator
+		$user = wp_get_current_user();
+		if(!user_can($user, 'manage_options')) return false;
+
+		if(!$redirect_uri) {
+			$redirect_uri = get_admin_url() . '?rfb_renew_access_token';
+		}
+
+		$fb = $this->get_fb_instance();
+
+		// Get Facebook User ID
+		$fb_user = $fb->getUser();
+
+		$location = $fb->getLoginUrl(array('redirect_uri' => $redirect_uri));
+
+		if(!headers_sent()) {
+		    header("Location: $location");
+		    exit;
+		} else {
+			echo '<script type="text/javascript">window.location = \''. $location .'\';</script>';
+			exit;
+		}
+
+		return ;
+	}
+
+	public function get_fb_instance() {
+		if(!self::$fb_instance) {
+
+			require dirname(__FILE__) . '/facebook-php-sdk/facebook.php';
+			
+			$opts = $this->get_options();
+			
+			self::$fb_instance = new Facebook(array(
+		   	 	'appId'  => trim($opts['app_id']),
+		    	'secret' => trim($opts['app_secret']),
+			));
+		}
+		return self::$fb_instance;
+	}
+
+	public function get_posts() {
+		$cache_file = dirname(__FILE__) . '/../cache/posts.cache';
+		$opts = $this->get_options();
+
+		// check if cache file exists 
+		// check if cache file exists for longer then the given expiration time
+		if(!file_exists($cache_file) || ($this->get_time_of_last_file_change($cache_file) < (time() - $opts['cache_time']))) {
+			$this->renew_cache_file();
+
+			if(!file_exists($cache_file)) return array();
+		}
+
+		$posts = json_decode(file_get_contents($cache_file), true);
+		return $posts;
+	}
+
+	public function renew_cache_file() {
+		$opts = $this->get_options();
+		if(empty($opts['app_id']) || empty($opts['fb_id'])) return false;
+
+		$access_token = get_option('rfb_access_token'); 
+
+		if(!$access_token) {
+			$access_token = $this->renew_access_token();
+			if(!$access_token) return false;
+		}
+
+		$fb = $this->get_fb_instance();
+		$fb->setAccessToken($access_token);
+
+		if(!$fb->getUser()) return false;
+
+		$apiResult = $fb->api(trim($opts['fb_id']) . '/posts?with=message&limit=250');
+		
+		if(!$apiResult or !is_array($apiResult) or !isset($apiResult['data']) or !is_array($apiResult['data'])) { return false; }
+
+		$data = array();
+		foreach($apiResult['data'] as $p) {
+			
+			// skip this "post" if it is not of one of the following types
+			if(!in_array($p['type'], array('status', 'photo', 'video', 'link'))) { 
+				continue;
+			}
+
+			// skip empty status updates
+			if($p['type'] == 'status' && empty($p['message'])) { continue; }
+
+			//split user and post ID (userID_postID)
+			$idArray = explode("_", $p['id']);
+			
+			$post = array();
+			$post['type'] = $p['type'];
+			$post['author'] = $p['from'];
+			$post['content'] = isset($p['message']) ? $p['message'] : '';
+			
+
+			// set post content and image
+			if($p['type'] == 'photo') { 
+				$post['image'] = "http://graph.facebook.com/".$p['object_id']."/picture";
+			} elseif($p['type'] == 'video') {
+				//$post['image'] = $p['picture'];
+				$post['image'] = "http://graph.facebook.com/".$p['object_id']."/picture";
+				$post['content'] .= "\n\n {$p['link']}";
+			} else {
+				$post['image'] = null;
+			}
+
+
+			// calculate post like and comment counts
+			if(isset($p['likes'])) {
+				if(isset($p['likes']['count'])) {
+					$like_count = $p['likes']['count'];
+				} elseif(isset($p['likes']['data']) && is_array($p['likes']['data'])) {
+					$like_count = count($p['likes']['data']);
+				}
+			} else {
+				$like_count = 0;
+			}
+
+			if(isset($p['comments'])) {
+				if(isset($p['comments']['count'])) {
+					$comment_count = $p['comments']['count'];
+				} elseif(isset($p['comments']['data']) && is_array($p['comments']['data'])) {
+					$comment_count = count($p['comments']['data']);
+				}
+			} else {
+				$comment_count = 0;
+			}
+
+			$post['timestamp'] = strtotime($p['created_time']);
+			$post['like_count'] = $like_count;
+			$post['comment_count'] = $comment_count;
+			$post['link'] = "http://www.facebook.com/".$opts['fb_id']."/posts/".$idArray[1];
+			$data[] = $post;
+
+		}
+
+		$data = json_encode($data);
+		$cache_dir = dirname(__FILE__) . '/../cache/';
+		$cache_file = dirname(__FILE__) . '/../cache/posts.cache';
+		
+		// create cache directory if not exists
+		if(!file_exists($cache_dir)) {
+			@mkdir($cache_dir, 0777);
+		}
+
+		// abandon if cache folder is not writable
+		if(!is_writable(dirname(__FILE__) . '/../cache/')) {
+			return false;
+		}
+
+		file_put_contents($cache_file, $data);
+
+		return true;
+	}
+
+	public function shortcode_output($atts)
+	{
+		 extract(shortcode_atts(array(
+	      'number' => '5',
+	      'likes' => 1,
+	      'comments' => 1,
+	      'excerpt_length' => 140
+    	 ), $atts));
+
+		$opts = $this->get_options();
+		$posts = $this->get_posts();
+		$posts = array_slice($posts, 0, $number);
+
+		$output = '<div class="recent-facebook-posts rfb_posts shortcode">';
+
+		foreach($posts as $post) { 
+			$content = $post['content'];
+			$shortened = false;
+
+			if(strlen($content) > $excerpt_length) {
+				$limit = strpos($post['content'], ' ',$excerpt_length); 
+				if($limit) {
+					$content = substr($post['content'], 0, $limit);
+					$shortened = true;
+				}
+			}
+		
+		
+			$output .= '<div class="rfb-post">';
+			$output .= '<p class="rfb_text">'. nl2br(make_clickable($content));
+			if ($shortened) $output .= '..';
+			$output .= '</p>';
+
+			if($opts['img_size'] != 'dont_show' && isset($post['image']) && $post['image']) { 
+				if(isset($post['type']) && $post['type'] == 'photo') {
+					$img_atts = 'src="'. $post['image'] .'?type='. $opts['img_size'] .'" style="max-width: '. $opts['img_width'] .'px; max-height: '. $opts['img_width'] .'px;"';	
+				} else {
+					$img_atts = 'src="'. $post['image'] .'" style="max-width: '. $opts['img_width'] .'px; max-height: '. $opts['img_width'] .'px;"';	
+				}
+
+				$output .= '<p class="rfb_image"><a target="_blank" href="'. $post['link'] . '" rel="nofollow"><img '. $img_atts .' alt="" /></a></p>';
+			}
+
+			$output .= '<p><a target="_blank" class="rfb_link" href="'. $post['link'] .'" rel="nofollow">';
+			if($likes || $comments) { $output .= '<span class="like_count_and_comment_count">'; }
+			if($likes) { $output .= '<span class="like_count">'. $post['like_count'] . ' <span>likes</span></span> '; }
+			if($comments) { $output .= '<span class="comment_count">' . $post['comment_count'] . ' <span>comments</span></span> '; }
+			if($likes || $comments) { $output .= '</span>'; }
+			$output .= '<span class="timestamp" title="'. date('l, F j, Y', $post['timestamp']) . ' at ' . date('G:i', $post['timestamp']) . '" >';
+			if($likes || $comments) { $output .= ' · '; }
+			$output .= '<span>' . $this->time_ago($post['timestamp']) . '</span></span>';
+			$output .= '</a></p></div>' ;
+		
+		} 
+
+		if(empty($posts)) {
+			$output .= '<p>No recent Facebook status updates to show.</p>';
+			if(current_user_can('manage_options')) { 
+				$output .= '<p><strong>Admins only notice:</strong> Did you <a href="' . get_admin_url(null,'options-general.php?page=rfb-settings') . '">configure the plugin</a> properly?</p>';
+			}
+		}
+
+		$output .= "</div>";
+		return $output;
+	}
+
+	public function time_ago($timestamp) {
+		$diff = time() - (int) $timestamp;
+
+	    if ($diff == 0) 
+	         return 'just now';
+
+	    $intervals = array
+	    (
+	        1                   => array('year',    31556926),
+	        $diff < 31556926    => array('month',   2628000),
+	        $diff < 2629744     => array('week',    604800),
+	        $diff < 604800      => array('day',     86400),
+	        $diff < 86400       => array('hour',    3600),
+	        $diff < 3600        => array('minute',  60),
+	        $diff < 60          => array('second',  1)
+	    );
+
+	    $value = floor($diff/$intervals[1][1]);
+	    return $value.' '.$intervals[1][0].($value > 1 ? 's' : '').' ago';
+	}	
+
+	private function get_time_of_last_file_change($filePath) 
+	{ 
+		clearstatcache();
+	    $time = filemtime($filePath); 
+
+	    $isDST = (date('I', $time) == 1); 
+	    $systemDST = (date('I') == 1); 
+
+	    $adjustment = 0; 
+
+	    if($isDST == false && $systemDST == true) 
+	        $adjustment = 3600; 
+	    
+	    else if($isDST == true && $systemDST == false) 
+	        $adjustment = -3600; 
+
+	    else 
+	        $adjustment = 0; 
+
+	    return ($time + $adjustment); 
+	} 
+	
+}
---
+++ readme.txt
@@ -0,0 +1,158 @@
+=== Plugin Name ===
+Contributors: DvanKooten
+Donate link: http://dannyvankooten.com/donate/
+Tags: facebook,posts,fanpage,recent posts,fb,like box alternative,widget,facebook widget,widgets,facebook updates,like button,fb posts
+Requires at least: 3.0.1
+Tested up to: 3.5.2
+Stable tag: 1.2.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Widget that lists your recent Facebook page or profile updates / posts. A faster/prettier/more customizable alternative to Facebooks Like Box.
+
+== Description ==
+
+This plugin adds a widget (and a shortcode `[recent-facebook-posts]`) to your WordPress installation which you can use to list your recent Facebook posts / updates. This can be either posts from your personal profile or posts from your fanpage.
+
+**Benefits of using Recent Facebook Posts**
+
+* More freedom then Facebooks Like Box, easier to customize and/or style by using plain old CSS.
+* Outputs valid HTML, no more iframes.
+* Faster then Facebooks like box, because the result is properly cached on your server.
+* SEO friendly, your Facebook page / profile content becomes part of your website content. No iframes or JavaScript is involved.
+
+**Demo**
+
+[Recent FB Posts demo](http://wpdemo.dannyvankooten.com/), the widget is located in the right sidebar. As you can see, you can make the widget blend in with your theme perfectly.
+
+**Coming up**
+
+* Option to show a Like button
+* Suggestions, anyone? Drop me a line if you do.
+
+**More info:**
+
+* [Recent Facebook Posts for WordPress](http://dannyvankooten.com/wordpress-plugins/recent-facebook-posts/)
+* Check out more [WordPress plugins](http://dannyvankooten.com/wordpress-plugins/) by Danny van Kooten
+* You should follow [Danny on Twitter](http://twitter.com/DannyvanKooten) for lightning fast support and updates.
+
+== Installation ==
+
+1. Upload the contents of the .zip-file to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. [Create a new Facebook application here](http://developers.facebook.com/apps). Fill in ANY name you'd like (it won't be visible to the public), leave the namespace field blank and the hosting checkbox unchecked.
+1. In the Basic Application settings screen, scroll down a bit and select "Website with Facebook login" and set your `Site URL` to the URL of your WordPress installation.
+1. Copy your `App ID` and `App Secret` (see screenshot 1)
+1. Go to your WordPress Admin panel and open the Recent Facebook Posts options screen. (Settings > Recent Facebook Posts)
+1. Paste your Facebook `App id` and `App Secret`.  [screenshot 1](http://wordpress.org/extend/plugins/recent-facebook-posts/screenshots/)
+1. Find the numeric Facebook ID of your public Facebook page with [this website](http://findmyfacebookid.com/).
+1. Fill in your Facebook ID in the RFB options screen.
+1. Test your configuration by clicking the button "Renew access token".
+1. Test if caching works by clicking the "Renew cache" button.
+1. Drag the 'Recent FB Posts Widget' to one of your widget areas.
+1. (optional) Apply some custom CSS rules to style your recent FB posts widget. Just add them to your theme's CSS file.
+
+If you're still in doubt, have a look at the [screenshots](http://wordpress.org/extend/plugins/recent-facebook-posts/screenshots/).
+
+== Frequently Asked Questions ==
+
+= What does this plugin do? =
+This plugin adds a widget to your WordPress installation which you can use to list your X most recent Facebook posts. This can be either posts from your personal profile or from one of your fanpages. An example of the widget can be found [here](http://wpdemo.dannyvankooten.com/).
+
+= Why not simply use FB's likebox? =
+This plugin gives you the freedom to style your most recent facebook posts using plain old CSS, thus giving you much more freedom. Also, this plugin outputs valid HTML. Iframes are a thing of the past. Your Facebook posts are cached on your server so it is somewhat faster too.
+
+= How to configure this plugin? =
+Create a new Facebook application and fill in the field where it asks for your website URL. Then go to the configuration page of Recent Facebook Posts and copy-paste your App ID and App Secret. Have a look at the screenshots if you're not clear about which fields you need.
+
+= Why do I need to create a Facebook application? =
+In order to query content on Facebook an application is needed. Facebook doesn't allow their content publicly to anyone, they want to know "who is asking". You don't need to submit your app to the App Center in order for it to work though.
+
+= Do you have a working demo I can take a look at? =
+Sure, [here](http://wpdemo.dannyvankooten.com/). The widget is located in the right sidebar and shows posts from [my Facebook page](http://www.facebook.com/DannyvanKootenCOM). "Like" it, while you're at it!
+
+= Facebook gives me this error when renewing the access token: The specified URL is not owned by the application =
+You are running the plugin on a different (sub)domain then specified in your FB app configuration. Fix it by correctly setting your "Site URL" or by adding an App Domain if you are running the plugin on a subdomain.
+
+= The plugin says it is connected, renewing my access token works but still there are no status updates to be shown. =
+Please check if the page you are trying to fetch posts from has **publicly** available posts. The privacy setting of your status updates has to be "everyone" in order for the plugin to "see" your posts.
+
+= Where to add custom CSS =
+IMO, appearance should be handled by the theme you are using. This is why your custom CSS rules should be added to your theme's stylesheet file. You can find this file by browsing (over FTP) to `/wp-content/themes/your-theme-name/style.css`, or you can just use the WP Editor under Appearance > Editor.
+
+= Does this plugin work with group posts? =
+Currently, no. This plugin currently only works with pages and personal profiles.
+
+= Can I show a list of recent facebook updates in my posts or pages? =
+Yes, you can use the `[recent-facebook-posts]` shortcode. Optionally, add the following attributes: `likes`, `comments`, `excerpt_length`, `number`. Example: `[recent-facebook-posts number=10 likes=0 comments=0 excerpt_length=250]`
+
+Valid attribute values are as follows:
+`likes` : 0 (don't show like count), 1 (show like count)
+`comments` : 0 (don't show comment count), 1 (show comment count)
+`excerpt_length` : 1 - 9999 (sets the length of the excerpt)
+`number` : 1 - 99 (set the number of posts to show)
+
+== Screenshots ==
+
+1. The black bordered circles are the fields you'll need to provide to Facebook, at a minimum. The green circled fields are the values you'll need to provide to Recent Facebook Posts.
+2. The green circled fields are the fields where you'll need to provide your Facebook app id and app secret (as shown in screenshot 1).
+
+== Changelog ==
+
+= 1.2.1 =
+* Added option to hide images
+* Added option to load either thumbnail or normal size images from Facebook's CDN
+* Added border to image links
+
+= 1.2.1 =
+* Fixed parameter app_id is required notice before being able to enter it.
+
+= 1.2 =
+* Fixed: Reverted back to 'posts' instead of 'feed', to exclude posts from others.
+* Fixed: undefined index 'count' when renewing cache file   
+* Fixed: wrong comment or like count for some posts
+* Improved: calculation of cache file modification time to prevent unnecessary cache renewal
+* Improved: error message when cURL is not enabled
+* Improved: access token and cache configuration options are now only available when connected
+
+= 1.1.2 =
+* Fixed: Added spaces after the like and comment counts in the shortcode output
+
+= 1.1.1 =
+* Updated: Expanded installation instructions.
+* Changed: Some code improvements
+* Added: Link to Facebook numeric ID helper website.
+* Added: Check if cache directory exists. If not the plugin will now automatically try to create it with the right permissions.
+* Added: option to open link to Facebook Page in a new window.
+
+= 1.1 =
+* Added: Shortcode to show a list of recent facebook updates in your posts: '[recent-facebook-posts]'
+
+= 1.0.5 =
+* Added: More user-friendly error message when cURL is not enabled on your server.
+
+= 1.0.4 =
+* Improved: The way the excerpt is created, words (or links) won't be cut off now
+* Fixed: FB API Error for unknown fields.
+* Added: Images from FB will now be shown too. Drop me a line if you think this should be optional.
+
+= 1.0.3 = 
+* Improved the way the link to the actual status update is created (thanks Nepumuk84).
+* Improved: upped the limit of the call to Facebooks servers. 
+
+= 1.0.2 =
+* Fixed a PHP notice in the backend area when renewing cache and fetching shared status updates.
+* Added option to show link to Facebook page, with customizable text.
+
+= 1.0.1 =
+* Added error messages for easier debugging.
+
+= 1.0 =
+* Added option to load some default CSS
+* Added option to show like count
+* Added option to show comment count
+* Improved usability. Configuring Recent Facebook Posts should be much easier now due to testing options.
+
+= 0.1 =
+* Initial release
+
