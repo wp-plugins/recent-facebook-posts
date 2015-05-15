d229fdbef8392512963025a03937bc78a62d052a
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 09:57:31 +0000

    version 2.0.6
    git-svn-id: https://plugins.svn.wordpress.org/recent-facebook-posts/tags/2.0.6@1161044 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-public.php
+++ includes/class-public.php
@@ -1,29 +1,44 @@
 <?php
 
+if( ! defined( 'RFBP_VERSION' ) ) {
+	exit;
+}
+
 class RFBP_Public {
 
-	private static $instance = null;
+	/**
+	 * @var array
+	 */
+	protected $options;
 
-	public static function instance() 
-	{
-		if(!self::$instance) {
+	protected static $instance;
+
+	/**
+	 * @return RFBP_Public
+	 */
+	public static function instance() {
+		if( ! self::$instance ) {
 			self::$instance = new RFBP_Public();
 		}
 
 		return self::$instance;
 	}
 
-	public function __construct() {
+	/**
+	 * Constructor
+	 */
+	private function __construct() {
+		$this->options = rfbp_get_settings();
+	}
 
-		$opts = rfbp_get_settings();
-
-		include_once RFBP_PLUGIN_DIR . 'includes/helper-functions.php';
-		include_once RFBP_PLUGIN_DIR . 'includes/template-functions.php';
-
+	/**
+	 * Add hooks
+	 */
+	public function add_hooks() {
 		add_shortcode( 'recent_facebook_posts', array( $this, 'output' ) );
 		add_shortcode( 'recent-facebook-posts', array( $this, 'output' ) );
 
-		if ( $opts['load_css'] ) {
+		if ( $this->options['load_css'] ) {
 			add_action( 'wp_enqueue_scripts', array( $this, 'load_css' ) );
 		}
 
@@ -33,163 +48,106 @@ class RFBP_Public {
 		add_filter( 'rfbp_content', 'wpautop' );
 	}
 
+	/**
+	 * Load CSS
+	 */
 	public function load_css() {
-		wp_register_style( 'recent-facebook-posts-css', plugins_url( 'recent-facebook-posts/assets/css/default.css' ), array(), RFBP_VERSION );
+		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+		wp_register_style( 'recent-facebook-posts-css', plugins_url( 'recent-facebook-posts/assets/css/default' . $suffix . '.css' ), array(), RFBP_VERSION );
 		wp_enqueue_style( 'recent-facebook-posts-css' );
 	}
 
-	public function get_fallback_posts() {
-		$posts = get_transient( 'rfbp_posts_fallback' );
-
-		if ( !$posts ) {
-			return array();
-		}
-
-		return $posts;
-	}
-
+	/**
+	 * Get posts
+	 *
+	 * @return array
+	 */
 	public function get_posts() {
 
 		// try to get posts from cache
-		if ( ( $posts = get_transient( 'rfbp_posts' ) ) ) {
+		$posts = json_decode( get_transient( 'rfbp_posts' ), true );
+		if ( is_array( $posts ) ) {
 			return $posts;
 		}
 
-		$opts = rfbp_get_settings();
-
+		// fetch posts from FB API
 		$api = rfbp_get_api();
-		$data = $api->get_posts();
+		$posts = $api->get_posts();
 
-		// did api call succeed?
-		if ( !$data ) {
-			return $this->get_fallback_posts();
+		if( is_array( $posts ) ) {
+
+			// API call succeeded, store posts in cache
+			// JSON encode posts before storing
+			$encoded_posts = json_encode( $posts );
+			set_transient( 'rfbp_posts', $encoded_posts, apply_filters( 'rfbp_cache_time', 3600 ) ); // user set cache time
+			set_transient( 'rfbp_posts_fallback', $encoded_posts, 2629744 ); // 1 month
+
+			return $posts;
 		}
 
-		$posts = array();
-		foreach ( $data as $p ) {
+		// API call failed, get posts from fallback transient
+		$posts = json_decode( get_transient( 'rfbp_posts_fallback' ), true );
 
-			// skip this "post" if it is not of one of the following types
-			if ( !in_array( $p->type, array( 'status', 'photo', 'video', 'link' ) ) ) {
-				continue;
-			}
-
-			// skip empty status updates
-			if ($p->type == 'status' && (!isset($p->message) || empty($p->message) ) ) {
-				continue;
-			} 
-
-			// skip empty links.
-			if ($p->type == 'link' && !isset($p->name) && (!isset($p->message) || empty($p->message))) {
-				continue;
-			}
-
-			// skip friend approvals
-			if ( $p->type == 'status' && $p->status_type == 'approved_friend' ) { continue; }
-
-			//split user and post ID (userID_postID)
-			$idArray = explode( "_", $p->id );
-
-			$post = array();
-			$post['type'] = $p->type;
-			$post['content'] = isset( $p->message ) ? utf8_encode( $p->message ) : '';
-			$post['image'] = null;
-
-			// set post content and image
-			if ( $p->type == 'photo' ) {
-
-				$image = "//graph.facebook.com/". $p->object_id . '/picture';
-				$post['image'] = $image;
-
-			} elseif ( $p->type == 'video' ) {
-
-				$image = $p->picture;
-
-				// hacky
-				if ( $opts['img_size'] == 'normal' ) {
-					$image = str_replace( array( "_s.jpg", "_s.png" ), array( "_n.jpg", "_n.png" ), $image );
-				}
-
-				$post['image'] = $image;
-
-			} elseif ( $p->type == 'link' ) {
-				$post['link_image'] = ( isset( $p->picture ) ) ? $p->picture : '';
-				$post['link_name'] = ( isset( $p->name ) ) ? $p->name : '';
-				$post['link_caption'] = ( isset( $p->caption ) ) ? $p->caption : '';
-				$post['link_description'] = ( isset( $p->description ) ) ? $p->description : '';
-				$post['link_url'] = $p->link;
-			}
-
-			// calculate post like and comment counts
-			if ( isset( $p->likes->summary->total_count ) ) {
-				$like_count = $p->likes->summary->total_count;
-			} else {
-				$like_count = 0;
-			}
-
-			if ( isset( $p->comments->summary->total_count ) ) {
-				$comment_count = $p->comments->summary->total_count;
-			} else {
-				$comment_count = 0;
-			}
-
-			$post['timestamp'] = strtotime( $p->created_time );
-			$post['like_count'] = $like_count;
-			$post['comment_count'] = $comment_count;
-			$post['url'] = "http://www.facebook.com/".$opts['fb_id']."/posts/".$idArray[1];
-			$posts[] = $post;
-
+		// If fallback also failed, return empty array
+		if( is_array( $posts ) ) {
+			return $posts;
 		}
 
-		// store results in cache for later use
-		if ( $posts ) {
-			set_transient( 'rfbp_posts', $posts, apply_filters( 'rfbp_cache_time', 3600 ) ); // user set cache time
-			set_transient( 'rfbp_posts_fallback', $posts, 2629744 ); // 1 month
-		}
+		return array();
 
-		return $posts;
 	}
 
+	/**
+	 * Outputs the HTML with the facebook posts
+	 *
+	 * @param array $atts
+	 *
+	 * @return string
+	 */
 	public function output( $atts = array() ) {
 
-		$opts = rfbp_get_settings();
+		$opts = $this->options;
 		$posts = $this->get_posts();
 
+		// upgrade from old `show_link` parameter.
 		if ( isset( $atts['show_link'] ) ) {
 			$atts['show_page_link'] = $atts['show_link'];
 		}
 
-		extract( shortcode_atts( array(
-					'number' => '5',
-					'likes' => 1,
-					'comments' => 1,
-					'excerpt_length' => 140,
-					'el' => 'div',
-					'origin' => 'shortcode',
-					'show_page_link' => false,
-					'show_link_previews' => ( $opts['load_css'] )
-				), $atts ) );
+		$defaults = array(
+			'number' => '5',
+			'likes' => 1,
+			'comments' => 1,
+			'excerpt_length' => 140,
+			'el' => 'div',
+			'origin' => 'shortcode',
+			'show_page_link' => false,
+			'show_link_previews' => $opts['load_css']
+		);
+
+		$atts = shortcode_atts( $defaults, $atts );
 
 		ob_start();
 ?>
-		<!-- Recent Facebook Posts v<?php echo RFBP_VERSION; ?> - http://wordpress.org/plugins/recent-facebook-posts/ -->
-		<div class="recent-facebook-posts rfbp rfbp-container rfbp-<?php echo $origin; ?>">
+		<!-- Recent Facebook Posts v<?php echo RFBP_VERSION; ?> - https://wordpress.org/plugins/recent-facebook-posts/ -->
+		<div class="recent-facebook-posts rfbp rfbp-container rfbp-<?php echo $atts['origin']; ?>">
 			<?php
 
-		if ( $posts && !empty( $posts ) ) {
+		if ( $posts && ! empty( $posts ) ) {
 
-			if ( $el == 'li' ) { echo '<ul class="rfbp-posts-wrap">'; }
+			if ( $atts['el'] == 'li' ) { echo '<ul class="rfbp-posts-wrap">'; }
 
-			$posts = array_slice( $posts, 0, $number );
+			$posts = array_slice( $posts, 0, $atts['number'] );
 			$link_target = ( $opts['link_new_window'] ) ? "_blank" : '';
 
 			foreach ( $posts as $p ) {
 
-				$content = utf8_decode($p['content']);
-
 				$shortened = false;
+				$content = $p['content'];
 
-				if ( strlen( $content ) > $excerpt_length ) {
-					$limit = strpos( $content, ' ', $excerpt_length );
+				// shorten content if it exceed the set excerpt length
+				if ( strlen( $content ) > $atts['excerpt_length'] ) {
+					$limit = strpos( $content, ' ', $atts['excerpt_length'] );
 					if ( $limit ) {
 						$content = substr( $content, 0, $limit );
 						$shortened = true;
@@ -197,7 +155,7 @@ class RFBP_Public {
 				}
 ?>
 
-					<<?php echo $el; ?> class="rfbp-post">
+					<<?php echo $atts['el']; ?> class="rfbp-post">
 					<div class="rfbp-text">
 
 						<?php
@@ -209,7 +167,7 @@ class RFBP_Public {
 
 					</div>
 
-					<?php if ( $show_link_previews && isset( $p['link_url'] ) && !empty( $p['link_url'] ) && !empty( $p['link_name'] ) ) { ?>
+					<?php if ( $atts['show_link_previews'] && isset( $p['link_url'] ) && !empty( $p['link_url'] ) && !empty( $p['link_name'] ) ) { ?>
 
 					<p class="rfbp-link-wrap">
 						<a class="rfbp-link" href="<?php echo $p['link_url']; ?>" rel="external nofollow" target="<?php echo $link_target; ?>">
@@ -220,47 +178,57 @@ class RFBP_Public {
 							<?php } ?>
 
 							<span class="rfbp-link-text-wrap">
-								<span class="rfbp-link-name"><?php echo $p['link_name']; ?></span>
-								<?php if ( isset( $p['link_caption'] ) ) { ?><span class="rfbp-link-caption"><?php echo $p['link_caption']; ?></span><?php } ?>
-								<?php if ( isset( $p['link_description'] ) && !empty( $p['link_description'] ) ) { ?><span class="rfbp-link-description"><?php echo $p['link_description']; ?></span><?php } ?>
+								<span class="rfbp-link-name"><?php echo esc_html( $p['link_name'] ); ?></span>
+								<?php if ( isset( $p['link_caption'] ) ) { ?><span class="rfbp-link-caption"><?php echo esc_html( $p['link_caption'] ); ?></span><?php } ?>
+								<?php if ( isset( $p['link_description'] ) && !empty( $p['link_description'] ) ) { ?><span class="rfbp-link-description"><?php echo esc_html( $p['link_description'] ); ?></span><?php } ?>
 							</span>
 						</a>
 					</p>
 
 					<?php } ?>
 
-					<?php if ( $opts['img_size'] != 'dont_show' && isset( $p['image'] ) && !empty( $p['image'] ) ) { ?>
+					<?php if ( $opts['img_size'] !== 'dont_show' && isset( $p['image'] ) && ! empty( $p['image'] ) ) {
+
+					// grab bigger video thumbnail (hacky)
+					if ( $p['type'] === 'video' && $opts['img_size'] == 'normal' ) {
+						$p['image'] = str_ireplace( array( "_s.jpg", "_s.png" ), array( "_n.jpg", "_n.png" ), $p['image'] );
+					}
+
+					?>
 					<p class="rfbp-image-wrap">
 						<a class="rfbp-image-link" target="<?php echo $link_target; ?>" href="<?php echo $p['url']; ?>" rel="external nofollow">
-							<?php $max_img_width = ( !empty( $opts['img_width'] ) ) ? $opts['img_width'].'px' : '100%'; $max_img_height = ( !empty( $opts['img_height'] ) ) ? $opts['img_height'].'px' : 'none'; ?>
-							<img class="rfbp-image" src="<?php echo esc_attr($p['image'] . '?type=' . $opts['img_size']); ?>" style="max-width: <?php echo $max_img_width; ?>; max-height: <?php echo $max_img_height; ?>" alt="" />
+							<?php $max_img_width = ( ! empty( $opts['img_width'] ) ) ? $opts['img_width'].'px' : '100%'; $max_img_height = ( !empty( $opts['img_height'] ) ) ? $opts['img_height'].'px' : 'none'; ?>
+							<img class="rfbp-image" src="<?php echo esc_attr( $p['image'] . '?type=' . $opts['img_size'] ); ?>" style="<?php echo esc_attr( "max-width: {$max_img_width}; max-height: {$max_img_height}" ); ?>" alt="" />
 						</a>
 					</p>
 					<?php } ?>
 					<p class="rfbp-post-link-wrap">
-						<a target="<?php echo $link_target; ?>" class="rfbp-post-link" href="<?php echo $p['url']; ?>" rel="external nofolloW">
-							<?php if ( $likes ) { ?><span class="rfbp-like-count"><?php echo $p['like_count']; ?> <span><?php _e( "likes", 'recent-facebook-posts' ); ?><?php if ( $comments ) { ?>, <?php } ?></span></span><?php } ?>
-							<?php if ( $comments ) { ?><span class="rfbp-comment-count"><?php echo $p['comment_count']; ?> <span><?php _e( "comments", 'recent-facebook-posts' ); ?></span></span><?php } ?>
-							<?php if ( $likes || $comments ) { ?> &sdot; <?php } ?>
+						<a target="<?php echo $link_target; ?>" class="rfbp-post-link" href="<?php echo esc_url( $p['url'] ); ?>" rel="external nofolloW">
+							<?php if ( $atts['likes'] ) { ?><span class="rfbp-like-count"><?php echo absint( $p['like_count'] ); ?> <span><?php _e( "likes", 'recent-facebook-posts' ); ?><?php if ( $atts['comments'] ) { ?>, <?php } ?></span></span><?php } ?>
+							<?php if ( $atts['comments'] ) { ?><span class="rfbp-comment-count"><?php echo absint( $p['comment_count'] ); ?> <span><?php _e( "comments", 'recent-facebook-posts' ); ?></span></span><?php } ?>
+							<?php if ( $atts['likes'] || $atts['comments'] ) { ?> &sdot; <?php } ?>
 							<span class="rfbp-timestamp" title="<?php printf( __( '%1$s at %2$s', 'recent-facebook-posts' ), date( 'l, F j, Y', $p['timestamp'] ), date( 'G:i', $p['timestamp'] ) ); ?>"><?php echo rfbp_time_ago( $p['timestamp'] ); ?></span>
 						</a>
 					</p>
-					</<?php echo $el; ?>>
+					</<?php echo $atts['el']; ?>>
 				<?php
 
 			} // end foreach $posts
 
-			if ( $el == 'li' ) { echo '</ul>'; }
+			// close list
+			if ( $atts['el'] === 'li' ) {
+				echo '</ul>';
+			}
 
 		} else {
 			?><p><?php _e( "No recent Facebook posts to show", 'recent-facebook-posts' ); ?></p><?php
 			if ( current_user_can( 'manage_options' ) ) {
-				?><p><strong><?php _e( "Admins only notice", 'recent-facebook-posts' ); ?>:</strong> Did you <a href="<?php echo admin_url( 'options-general.php?page=rfbp' ); ?>">configure the plugin</a> properly?</p><?php
+				?><p><strong><?php _e( "Admins only notice", 'recent-facebook-posts' ); ?>:</strong> <?php printf( __( 'Did you <a href="%s">configure the plugin</a> properly?', 'recent-facebook-posts' ), admin_url( 'options-general.php?page=rfbp' ) ); ?></p><?php
 			}
 		} ?>
 
-			<?php if ( $show_page_link ) { ?>
-				<p class="rfbp-page-link-wrap"><a class="rfbp-page-link" href="http://www.facebook.com/<?php echo $opts['fb_id']; ?>/" rel="external nofollow" target="<?php echo $link_target; ?>"><?php echo $opts['page_link_text']; ?></a></p>
+			<?php if ( $atts['show_page_link'] ) { ?>
+				<p class="rfbp-page-link-wrap"><a class="rfbp-page-link" href="<?php echo esc_url( 'https://www.facebook.com/' . $opts['fb_id'] . '/' ); ?>" rel="external nofollow" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $opts['page_link_text'] ); ?></a></p>
 			<?php } ?>
 
 			</div>
